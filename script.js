e76d707310a79ea3d55a25421b099bd67210f148
lebecasa <148072673+lebecasa@users.noreply.github.com>
2024-05-28 10:01:40 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+const caixaPrincipal= document.querySelector(".caixa-principal");
+const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultado= document.querySelector(".caixa-resulttado");
+const textoResultado= document.querySelector(".texto-resultado");
 
+const perguntas= [
+    {
+        enunciado:"Pergunta1",
+        alternativas:[
+            "alternativa1",
+            "alternativa2",
+        ]
+    },
+]
+const perguntas= [
+    {
+        enunciado:"Pergunta1",
+        alternativas:[
+            "alternativa1",
+            "alternativa2",
+        ]
+    },
+]
+const perguntas= [
+    {
+        enunciado:"Pergunta1",
+        alternativas:[
+            "alternativa1",
+            "alternativa2",
+        ]
+    },
+]
+const perguntas= [
+        {
+            enunciado:"Pergunta1",
+            alternativas:[
+                "alternativa1",
+                "alternativa2",
+            ]
+        },
+    ]
